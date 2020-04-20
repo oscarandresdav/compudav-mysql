742b1ced09bffcdef9c5b44d6944b8921d0fb972
Oscar Davila <oscar7822002@gmail.com>
2020-04-20 17:20:53 -0500

    TypeORM Integration
    npm install --save @nestjs/typeorm typeorm mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'k180b036',
+      database: 'compudavdb',
+      entities: [],
+      synchronize: true,
+      keepConnectionAlive: true,
+      retryAttempts: 2,
+      retryDelay: 1000,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
